124c683106e3a9047fcf6c2e80ddf50eede942a5
Kinyua <alfkinyua@gmail.com>
2022-12-08 14:27:03 +0300

    fix eslint errors

--- style.js
+++ style.js
@@ -1,10 +1,20 @@
 const bg = document.querySelector('.bg');
 const loadText = document.querySelector('.loading-text');
+// eslint-disable-next-line max-len
+const scale = (num, inMin, inMax, outMin, outMax) =>
+  ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+
 let load = 0;
-const int = setInterval(blurr, 30);
+let int;
+
 function blurr() {
+  load += 1;
+
   if (load > 99) {
     clearInterval(int);
   }
-  console.log(load);
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
+int = setInterval(blurr, 30);
